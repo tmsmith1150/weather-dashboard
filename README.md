1f7c133572337613050cb14f1b2ff5c35b998921
Michelle Jordan <tmsmith1150@live.com>
2020-05-27 16:59:39 -0400

    updated homework 5.27

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # weather-dashboard
+
+
+
+https://images.unsplash.com/photo-1586278058580-417054662af7?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=60
+
+https://images.unsplash.com/photo-1570379510114-eeb67b4e50e1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=60
--- script.js
+++ script.js
@@ -1,111 +1,125 @@
 
+var APIKey = "727c1e4e4fba7ce8daac9b0d1f5f503c";
+var cityName = $("#cityInput").val().trim()
 
-// function renderButtons() {
+// var citiesBtns = []
 
-// $("#city-search").on("click", function(event) {
-//     event.preventDefault();
-    
-//     var a = $("<button>");
-//     $("#buttons-view").append(a);
+//Here we run our AJAX call to the OpenWeatherMap API
+function handleClick() {
+  var cityName = $("#cityInput").val().trim()
+//Here we build the URLwe need to query the database
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey
 
-// });
-
-// renderButtons()
-
-
-
-    // constructing a queryURL variable we will use instead of the literal string inside of the ajax method
-    var city = "charlotte";
-    var queryURL = "api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid={727c1e4e4fba7ce8daac9b0d1f5f503c}";
-
-    
+$.ajax({
+  url: queryURL,
+  method: "GET"
 
 
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(response) {
-      console.log(response);
-      console.log(response.Runtime);
-    });
-// ---------------------------------------------------------
-
-    console.log("This console.log will probably happen first because of asynchronicity.");
-    var x = 2;
-    var y = 10;
-    var z = 13;
-    console.log("We can also assign some variables and do some arithmetic while we wait too: 2 + 10 + 13 = ", x + y + z);
+}).then(function(response) {
 
 
 
-// function displayWeatherInfo() {
-    
+  //Log the query
+  console.log(queryURL);
+  console.log(response);
+  console.log("Temp " + convertKelvin(response.main.temp) + " F");
+  console.log("TempLow " + convertKelvin(response.main.temp_min) + " F");
+  console.log("TempHigh " + convertKelvin(response.main.temp_max) + " F");
+
+  // var nameOfCity = (response.city.name);
+  // var pOne = $("<p>").text("Weather: " + name);
+  // cityDiv.append(pOne);
+  
+  //Storing the weather data
+  cityDiv = $("#cityDiv");
+  cityDiv2 = $("#cityDivDay2");
+  cityDiv3 = $("#cityDivDay3");
+  cityDiv4 = $("#cityDivDay4");
+  cityDiv5 = $("#cityDivDay5");
+  // Creating an element to have the weather displayed (list is not defined ref error in console so I changed to Array)
+  var temp = $("<p>").text("Current Temp " + convertKelvin(response.main.temp) + " F");
+  var tempLow = $("<p>").text("TempLow " + convertKelvin(response.main.temp_min) + " F");
+  var tempHigh = $("<p>").text("TempHigh " + convertKelvin(response.main.temp_max) + " F");
+  var humidity = $("<p>").text("Humidity " + response.main.humidity + "%");
+  // Displaying the weather
+  cityDiv.append(temp, tempLow, tempHigh, humidity);
+
+  var temp = $("<p>").text("Current Temp " + convertKelvin(response.main.temp) + " F");
+  var tempLow = $("<p>").text("TempLow " + convertKelvin(response.main.temp_min) + " F");
+  var tempHigh = $("<p>").text("TempHigh " + convertKelvin(response.main.temp_max) + " F");
+  var humidity = $("<p>").text("Humidity " + response.main.humidity + "%");
+  cityDiv2.append(tempLow, tempHigh, humidity);
+
+  var temp = $("<p>").text("Current Temp " + convertKelvin(response.main.temp) + " F");
+  var tempLow = $("<p>").text("TempLow " + convertKelvin(response.main.temp_min) + " F");
+  var tempHigh = $("<p>").text("TempHigh " + convertKelvin(response.main.temp_max) + " F");
+  var humidity = $("<p>").text("Humidity " + response.main.humidity + "%");
+  cityDiv3.append(tempLow, tempHigh, humidity);
+
+  var temp = $("<p>").text("Current Temp " + convertKelvin(response.main.temp) + " F");
+  var tempLow = $("<p>").text("TempLow " + convertKelvin(response.main.temp_min) + " F");
+  var tempHigh = $("<p>").text("TempHigh " + convertKelvin(response.main.temp_max) + " F");
+  var humidity = $("<p>").text("Humidity " + response.main.humidity + "%");
+  cityDiv4.append(tempLow, tempHigh, humidity);
+
+  var temp = $("<p>").text("Current Temp " + convertKelvin(response.main.temp) + " F");
+  var tempLow = $("<p>").text("TempLow " + convertKelvin(response.main.temp_min) + " F");
+  var tempHigh = $("<p>").text("TempHigh " + convertKelvin(response.main.temp_max) + " F");
+  var humidity = $("<p>").text("Humidity " + response.main.humidity + "%");
+  cityDiv5.append(tempLow, tempHigh, humidity);
+
+  
+})};
+
+$("#add-city").on("click", handleClick)
+
+function convertKelvin(num) {
+  return parseInt(((((num) -273.15) * 1.8) +32))
+}
 
 
-//     var user = localStorage["#city-input"] ;
-//     document.getElementById("city-input").value = user ;
 
-//     var newCity = $("#city-input").val();
-//     var queryURL = "api.openweathermap.org/data/2.5/forecast?q={city name}&appid={727c1e4e4fba7ce8daac9b0d1f5f503c}" + newCity + "&apikey=trilogy";
-//     console.log(newCity);
+var citiesBtns = [];
+function renderButtons() {
+  
+  // Deleting the cities prior to adding new cities
+  // (this is necessary otherwise you will have repeat buttons)
+  $("#buttons-view").empty();
 
-//     $("#city-input").text(JSON.stringify(response));
+  // Looping through the array of cities
+  for (var i = 0; i < citiesBtns.length; i++) {
 
-// });
-//  displayWeatherInfo()   
+    // Then dynamicaly generating buttons for each city in the array
+    // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
+    var a = $("<button>");
+    // Adding a class of movie-btn to our button
+    a.addClass("city-btn");
+    // Adding a data-attribute
+    a.attr("data-name", citiesBtns[i]);
+    // Providing the initial button text
+    a.text(citiesBtns[i]);
+    // Adding the button to the buttons-view div
+    $("#buttons-view").append(a);
+  }
+}
+// This function handles events where a city button is clicked
+$("#add-city").on("click", function(event) {
+  event.preventDefault();
+  // This line grabs the input from the textbox
+  var city = $("#cityInput").val().trim();
 
+  // Adding city from the textbox to our array
+  citiesBtns.push(city);
 
-
-
-
-//     // Creates AJAX call for the specific movie button being clicked
-//     $.ajax({
-//       url: queryURL,
-//       method: "GET"
-//     }).then(function(response) {
-//         console.log(response);
-//         $("#weather-view").text(JSON.stringify(response));
-
-//       // Creates a div to hold the movie
-//       // Retrieves the Rating Data
-//       // Creates an element to have the rating displayed
-//       // Displays the rating
-//       // Retrieves the release year
-//       // Creates an element to hold the release year
-//       // Displays the release year
-//       // Retrieves the plot
-//       // Creates an element to hold the plot
-//       // Appends the plot
-//       // Creates an element to hold the image
-//       // Appends the image
-//       // Puts the entire Movie above the previous movies.
-//     });
-
-//   }
-
-//   function renderButtons() {
-
-//     // Deletes the movies prior to adding new movies
-//     // (this is necessary otherwise you will have repeat buttons)
-//     $("#buttons-view").empty();
-//     // Loops through the array of movies
-//     for (var i = 0; i < newCity.length; i++) {
+  // Calling renderButtons which handles the processing of our city array
+  renderButtons();
+});
 
 
 
 
 
 
-//       // Then dynamicaly generates buttons for each movie in the array
-//       // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
-//       var a = $("<button>");
-//       // Adds a class of movie to our button
-//       a.addClass("newBtn");
-//       // Added a data-attribute
-//       a.attr("data-name", newCity[i]);
-//       // Provided the initial button text
-//       a.text(newCity[i]);
-//       // Added the button to the buttons-view div
-//       $("#buttons-view").append(a);
-//     }
-//   }
+
+
+
