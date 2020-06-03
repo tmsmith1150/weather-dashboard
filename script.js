07a5868f8e658963d5f22993f133be304bf06b78
Michelle Jordan <tmsmith1150@live.com>
2020-06-03 15:58:01 -0400

    updated script to show dates

--- script.js
+++ script.js
@@ -5,14 +5,32 @@
 
 
 var APIKey = "727c1e4e4fba7ce8daac9b0d1f5f503c";
-var cityName = $("#cityInput").val().trim()
 
+var day1 = $(".h3-2").text(moment().add(1, 'days').format('l'));
+var day2 = $(".h3-3").text(moment().add(2, 'days').format('l'));
+var day3 = $(".h3-4").text(moment().add(3, 'days').format('l'));
+var day4 = $(".h3-5").text(moment().add(4, 'days').format('l'));
+
+cityDiv = $("#cityDiv");
+  cityDiv2 = $("#cityDivDay2");
+  cityDiv3 = $("#cityDivDay3");
+  cityDiv4 = $("#cityDivDay4");
+  cityDiv5 = $("#cityDivDay5");
+
+  cityDiv.hide();
+  cityDiv2.hide();
+  cityDiv3.hide();
+  cityDiv4.hide();
+  cityDiv5.hide();
+
+
+
+  // var cityName = $("#cityInput").val().trim()
 // var citiesBtns = []
 
 //Here we run our AJAX call to the OpenWeatherMap API
-function handleClick() {
-  var cityName = $("#cityInput").val().trim()
-
+function handleClick(cityName) {
+  cityName = $("#cityInput").val().trim()
   $("#add-city").click(function(){
     $(".current").empty();
     $(".forecast").empty();
@@ -65,14 +83,14 @@ $.ajax({
   console.log("TempLow " + convertKelvin(result.daily[0].temp.min) + " F");
   console.log("TempHigh " + convertKelvin(result.daily[0].temp.max) + " F");
 
-  
+  cityDiv.show();
+  cityDiv2.show();
+  cityDiv3.show();
+  cityDiv4.show();
+  cityDiv5.show();
   
   //Storing the weather data
-  cityDiv = $("#cityDiv");
-  cityDiv2 = $("#cityDivDay2");
-  cityDiv3 = $("#cityDivDay3");
-  cityDiv4 = $("#cityDivDay4");
-  cityDiv5 = $("#cityDivDay5");
+  
   // Creating an element to have the weather displayed (list is not defined ref error in console so I changed to Array)
   var temp = $("<p>").text("Current Temp " + convertKelvin(result.current.temp) + " F");
   var tempLow = $("<p>").text("Today's Low " + convertKelvin(result.daily[0].temp.min) + " F");
@@ -83,36 +101,37 @@ $.ajax({
 
   
   // var day = $(".h3-5").text(result.daily[1].dt);
+  
   var tempLow = $("<p>").text("TempLow " + convertKelvin(result.daily[1].temp.min) + " F");
   var tempHigh = $("<p>").text("TempHigh " + convertKelvin(result.daily[1].temp.max) + " F");
   var humidity = $("<p>").text("Humidity " + result.daily[1].humidity + "%");
-  cityDiv2.append(tempLow, tempHigh, humidity);
- $(".h3-2").text(moment().add(1, 'days').format('l'));
-  
+  cityDiv2.append(day1, tempLow, tempHigh, humidity);
+//  $(".h3-2").text(moment().add(1, 'days').format('l'));
+ 
 
   
   // var day = $(".h3-3").text(result.daily[2].dt);
   var tempLow = $("<p>").text("TempLow " + convertKelvin(result.daily[2].temp.min) + " F");
   var tempHigh = $("<p>").text("TempHigh " + convertKelvin(result.daily[2].temp.max) + " F");
   var humidity = $("<p>").text("Humidity " + result.daily[2].humidity + "%");
-  cityDiv3.append(tempLow, tempHigh, humidity);
-  $(".h3-3").text(moment().add(2, 'days').format('l'));
+  cityDiv3.append(day2, tempLow, tempHigh, humidity);
+  
 
   
   // var day = $(".h3-4").text(result.daily[3].dt);
   var tempLow = $("<p>").text("TempLow " + convertKelvin(result.daily[3].temp.min) + " F");
   var tempHigh = $("<p>").text("TempHigh " + convertKelvin(result.daily[3].temp.max) + " F");
   var humidity = $("<p>").text("Humidity " + result.daily[3].humidity + "%");
-  cityDiv4.append(tempLow, tempHigh, humidity);
-  $(".h3-4").text(moment().add(3, 'days').format('l'));
+  cityDiv4.append(day3, tempLow, tempHigh, humidity);
+  
 
 
   // var day = $(".h3-2").text(result.daily[4].dt);
   var tempLow = $("<p>").text("TempLow " + convertKelvin(result.daily[4].temp.min) + " F");
   var tempHigh = $("<p>").text("TempHigh " + convertKelvin(result.daily[4].temp.max) + " F");
   var humidity = $("<p>").text("Humidity " + result.daily[4].humidity + "%");
-  cityDiv5.append(tempLow, tempHigh, humidity);
-  $(".h3-5").text(moment().add(4, 'days').format('l'));
+  cityDiv5.append(day4, tempLow, tempHigh, humidity);
+  
 
   
 })
@@ -163,7 +182,11 @@ function renderButtons() {
   }
 }
 
-
+$(document).on("click", ".city-btn", function(event) {
+  let val = $(this).attr("data-name");
+  handleClick(val)
+  console.log(val);
+});
 
 
 // This function handles events where a city button is clicked
